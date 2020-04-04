de159e09ae30fb990c7c9715491ec2760493683b
Mauricio Garduño <mau.gar.gnz@icloud.com>
2020-04-04 01:35:28 -0600

    axios rest demo

--- flixme/src/pages/Home.tsx
+++ flixme/src/pages/Home.tsx
@@ -1,8 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonCard } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonCard, IonList, IonItem, IonButton } from '@ionic/react';
 import React from 'react';
+import axios from 'axios';
 import './Home.css';
 
+const API_KEY = '2f11a380e1e347fda2aa2861bdd39f20';
+const  endpoint  = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`;
+
+const sendGetRequest = () => {
+
+  return axios({
+    url: endpoint,
+    method: 'get'
+  }).then(response => {
+
+    console.log(response);
+    return response.data;
+  })
+};
+
 const Home: React.FC = () => {
+
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+
+    sendGetRequest().then(data => setItems(data.articles));
+
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -25,6 +50,21 @@ const Home: React.FC = () => {
             </IonCol>
           </IonRow>
         </IonGrid>
+        <IonList color="primary">
+
+{
+  items.map(item => {
+
+    return (
+      <IonItem>
+        {item['title']}
+        <IonButton href={item['url']} color="primary" slot="end">See article</IonButton>
+      </IonItem>
+    );
+  })
+}
+
+</IonList>
       </IonContent>
     </IonPage>
   );
